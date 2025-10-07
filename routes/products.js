c6ec0d44b2592d89d8b590ec6c2dd1445ef782d5
Agbetile Amos <timilehinamos@gmail.com>
2025-10-07 18:41:02 +0100

    Added product grid and images

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 
-const products = [
-    { id: 1, name: "T-Shirt", price: 20 },
-    { id: 2, name: "Sneakers", price: 60 },
-    { id: 3, name: "Bag", price: 35 },
-];
+const products = JSON.parse(fs.readFileSync("./data/products.json", "utf-8"));
 
 router.get("/", (req, res) => {
-    res.render("products", { title: "Products", products });
+    const category = req.query.category;
+    const filtered = category
+        ? products.filter((p) => p.category === category)
+        : products;
+    res.render("products", { title: "Shop", products: filtered });
 });
 
 router.post("/add", (req, res) => {
     const { id } = req.body;
-    const product = products.find(p => p.id == id);
+    const product = products.find((p) => p.id == id);
     if (!req.session.cart) req.session.cart = [];
     req.session.cart.push(product);
     res.redirect("/cart");
